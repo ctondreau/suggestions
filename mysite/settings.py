af9db85767096b2712ce18901ec7e58c07ce78a6
Clay Tondreau <47112837+ctondreau@users.noreply.github.com>
2019-03-31 16:41:02 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,6 +57,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
